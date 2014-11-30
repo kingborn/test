85b880dd476fcd5dd3bce51f86a12ba5e528f546
tecweb2011 <simone.starace@studenti.uniparthenope.it>
2014-11-30 19:47:05 +0100

    Novita da SPAM PRODUCTION

--- prova.java
+++ prova.java
@@ -3,7 +3,7 @@ public class prova {
 	static void main( String args[] ) {
 	
 		System.out.println("PIER MASTER REN SLAVE");
-
+		System.out.println("Prossimamente un episodio speciale di Slave vs Master Reborn");
 	}
 
 }

1cbeccae7039cf187424babe7205eb9ed6cc4cc0
tecweb2011 <simone.starace@studenti.uniparthenope.it>
2014-11-30 20:00:37 +0100

    Ho modificato i lfile

--- prova.java
+++ prova.java
@@ -1,9 +1,16 @@
 public class prova {
 
+	public void run()
+	{
+		System.out.println("Non dar retta a Pierpaolo. Pierpaolo e strunz. Lui non e' il vero master. Sei tu il vero Master");
+	}
+
 	static void main( String args[] ) {
 	
 		System.out.println("PIER MASTER REN SLAVE");
 		System.out.println("Prossimamente un episodio speciale di Slave vs Master Reborn");
+		prova p = new prova();
+		p.run();
 	}
 
 }
